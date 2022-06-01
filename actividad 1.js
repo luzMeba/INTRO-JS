139d18efc04fead76d266770c8e918538742ed17
luzMeba <104604493+luzMeba@users.noreply.github.com>
2022-05-31 22:14:49 -0500

    Add files via upload

---
+++ actividad 1.js
@@ -0,0 +1,21 @@
+programa 1
+var num="ingresa un numero"
+r=num*2
+console.log(multiplicacion)
+
+programa 2
+var num1="ingresa un numero"
+var num2="ingresa otro numero"
+console.log(num1-num2)
+
+programa 3
+var radio= 5.3
+const = 3.1416
+var area(const*radio**2)
+console.log(area)
+
+programa 4
+var D=8
+var d=6
+var area(D*d/2)
+console.log(area)
